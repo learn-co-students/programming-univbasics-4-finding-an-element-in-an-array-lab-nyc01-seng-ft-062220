0a2f80b26728bf6a89e0164b62da9fb280f65e9a
Aian Soktoev <soktoevayan@gmail.com>
2020-05-28 21:07:23 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,25 @@
+#a=[1,2,3,4,5,6,7,8,9]
+
+#def find_element_index(array, value_to_find)
+ #bul = array.include?(value_to_find)
+  #if bul==true
+    #puts value_to_find
+    #return array.index(value_to_find)
+  #else
+    #return nil 
+  #end   
+#end
+
 def find_element_index(array, value_to_find)
-  # Add your solution here
-end
+  i=0 
+  k = value_to_find
+  while i<array.length do
+    if array[i]==k
+      number=i 
+      i+=1 
+    else 
+      i+=1 
+    end
+  end
+  return number
+end  
